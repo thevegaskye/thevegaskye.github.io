7a926657aebfe60e3f6d60a1e82c88a6a87c0a64
Kevin Shi <shikev@umich.edu>
2023-06-04 13:50:47 -0700

    routing changes completed

--- src/App.js
+++ src/App.js
@@ -1,27 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import "./App.css";
+import PortfolioComingSoon from "./pages/PortfolioComingSoon";
+import CaseStudySteward from "./pages/case-studies/CaseStudySteward";
 
 function App() {
   return (
-    <>
-    <header class="header">
-            <img class="header-logo" src="/vega-logo.png" />
-            <span class="header-text">DESIGN PORTFOLIO</span>
-        </header>
-        <section class="body-container">
-            <img class="illustration" src="/lilith.png" />
-            <div class="text-content">
-                <div class="coming-soon-container">
-                    <img class="desktop-coming-soon" src="/desktop-coming-soon.svg" />
-                    <img class="mobile-coming-soon" src="/mobile-coming-soon.svg" />
-                </div>
-                <div>
-                    <p>I am in an Age of Exploration — and I always will be. Driven by curiosity, adventure, and community my entire life, I approach design as an opportunity to craft meaningful experiences for users.</p>
-                    <p>Check back to see my UI/UX and visual design portfolio, you won't regret it!</p>
-                </div>
-            </div>
-        </section>
-        </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<PortfolioComingSoon />} />
+        <Route path="/steward" element={<CaseStudySteward />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ src/pages/case-studies/CaseStudySteward.js
@@ -0,0 +1,3 @@
+export default function CaseStudySteward() {
+  return <div>Steward</div>;
+}
