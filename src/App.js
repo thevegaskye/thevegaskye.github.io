4eed941b9be7932b3a532aeb7f8200dba30a3c9a
Kevin Shi <shikev@umich.edu>
2023-06-04 12:30:50 -0700

    move stuff over

--- src/App.js
+++ src/App.js
@@ -3,22 +3,25 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React test
-        </a>
-      </header>
-    </div>
+    <>
+    <header class="header">
+            <img class="header-logo" src="/vega-logo.png" />
+            <span class="header-text">DESIGN PORTFOLIO</span>
+        </header>
+        <section class="body-container">
+            <img class="illustration" src="/lilith.png" />
+            <div class="text-content">
+                <div class="coming-soon-container">
+                    <img class="desktop-coming-soon" src="/desktop-coming-soon.svg" />
+                    <img class="mobile-coming-soon" src="/mobile-coming-soon.svg" />
+                </div>
+                <div>
+                    <p>I am in an Age of Exploration — and I always will be. Driven by curiosity, adventure, and community my entire life, I approach design as an opportunity to craft meaningful experiences for users.</p>
+                    <p>Check back to see my UI/UX and visual design portfolio, you won't regret it!</p>
+                </div>
+            </div>
+        </section>
+        </>
   );
 }
 
